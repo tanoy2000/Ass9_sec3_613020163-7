ae5e89cf0954d4183b2a17960a90769889ed72d3
Nutnichachat <jannutnicha@kkumail.com>
2020-10-18 22:40:35 +0700

    Ass9_sec3_613020163-7

---
+++ server.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// default route 
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'Test Student Web API' })
+});
+// connection configurations 
+var dbConn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'lab_connect_mysql'
+});
+// connect to database dbconn.connect();
+dbConn.connect();
+app.get('/allstd', function (req, res) {
+    dbConn.query('SELECT * FROM student', function (error, results, fields) {
+        if (error) throw error;
+        return res.send(results);
+    });
+});
+
+app.post('/std', function (req, res) {
+    var std = req.body
+    if (!std) {
+        return res.status(400).send({ error: true, messge: 'Please provide student ' });
+
+    }
+    dbConn.query("INSERT INTO student SET ?", std, function (error, results, fielde) {
+        if (error) throw error;
+        return res.send(results);
+    });
+});
+app.get('/std/:id', function(req,res){
+    var std_id = req.params.id;
+    if (!std_id){
+        return res.status(400).send({ error: true, message:"Please provide student id"});
+    }
+    dbConn.query('SELECT * FROM student WHERE std_id = ?',std_id, function (error, results, fields) {
+        if (error) throw error;
+        if(results[0]){
+            return res.send(results[0]);
+        }else{
+            return res.status(400).send({ error: true, message: 'Student id not found!!' });
+        }
+    });
+})
+app.put('/std/:id', function (req,res) {
+    var std_id = req.params.id;
+    var std = req.body
+    if (!std_id || !std){
+        return res.status(400).send({ error: true, message: 'Please provide student id and student data'});
+    }
+    dbConn.query('UPDATE student SET ? WHERE std_id = ?', [std, std_id], function(error, results,fields) {
+        if (error) throw error;
+        return res.send({ error: false, message: 'Student has been updated seccessfully'});
+    });
+})
+app.delete('/std/:id', function(req,res){
+    var std_id = req.params.id;
+    if (!std_id){
+        return res.status(400).send({ error: true, message:"Please provide student id"});
+    }
+    dbConn.query('DELETE FROM student WHERE std_id = ?',std_id, function (error, results, fields) {
+        if (error) throw error;
+        return res.status(400).send({ error: true, message: 'Student has been updated successfully' });
+    });
+})
+
+
+app.listen(3000, function () {
+    console.log('Node app is running on port 3000');
+});
+module.exports = app;
---
+++ student.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 18, 2020 at 05:39 PM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.3.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `lab_connect_mysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `NO` int(11) NOT NULL,
+  `std_id` varchar(12) NOT NULL,
+  `std_name` varchar(50) NOT NULL,
+  `std_age` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`NO`, `std_id`, `std_name`, `std_age`) VALUES
+(35, '55555555-5', 'ta', 50),
+(36, '66666666-6', 'noy', 40),
+(37, '77777777-7', 'tanoy', 30);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`NO`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `NO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
